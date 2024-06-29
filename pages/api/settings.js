e2411b08881c19d3bb2c1c4aa89742901043e5d6
Berkay Orhan <beror658@student.liu.se>
2024-06-29 20:46:44 +0200

    refactor: Update settings API to use Auth0 for authentication
    This commit updates the settings API to use the Auth0 authentication library instead of the previous authentication method. The code changes include importing the necessary dependencies from the '@auth0/nextjs-auth0' package, updating the session retrieval logic, and using the 'user.sub' property as the user ID from Auth0. This change improves the security and user experience of the application.

--- pages/api/settings.js
+++ pages/api/settings.js
@@ -1,16 +1,16 @@
 // pages/api/settings.js
-import { getSession } from 'next-auth/react';
+import { getSession } from '@auth0/nextjs-auth0';
 import { connectToDatabase } from '../../utils/mongodb';
 import bcrypt from 'bcrypt';
 
 const settingsHandler = async (req, res) => {
-  const session = await getSession({ req });
-  if (!session) {
+  const { user } = await getSession(req, res);
+  if (!user) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
   const { db } = await connectToDatabase();
-  const userId = session.user.id;
+  const userId = user.sub;  // This is the user ID from Auth0
   const { newsletter, name, email, phone, password, notifications } = req.body;
 
   const update = {
